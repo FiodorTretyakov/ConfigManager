e8fe97c94ae08308650b8d96829e1a1265f1ef8e
Fiodor Tretyakov <fiodor.tretyakov@gmail.com>
2018-11-09 16:32:26 +0000

    Moved build script level up.

---
+++ build.ps1
@@ -0,0 +1,13 @@
+$runtime = "ubuntu.14.04-x64"
+
+$projectName = "ConfigManager"
+[xml]$xml = Get-Content "$projectName\$projectName.csproj"
+[int]$suffix = $xml.Project.PropertyGroup.VersionSuffix
+$prefix = $xml.Project.PropertyGroup.VersionPrefix
+$suffix += 1
+$xml.Project.PropertyGroup.VersionSuffix = [string]$suffix
+$xml.Save("$projectName\$projectName.csproj")
+
+cd $projectName
+dotnet deb -c Release -r $runtime -f netcoreapp2.1
+Copy-Item ".\bin\Release\$runtime\$projectName.$prefix-$suffix.$runtime.deb" "..\builds\"
