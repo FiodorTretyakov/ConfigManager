867829ec333e261ba448b23d92dc5c0359d26780
Fiodor Tretyakov <fiodor.tretyakov@gmail.com>
2018-11-09 16:05:43 +0000

    Added ConfigManager link.

--- ConfigManager/Terminal.cs
+++ ConfigManager/Terminal.cs
@@ -12,7 +12,7 @@ namespace ConfigManager
 
         public Terminal()
         {
-            _config = new ConfigurationBuilder().AddXmlFile("ConfigurationManager/Configuration.csproj").Build();
+            _config = new ConfigurationBuilder().AddXmlFile("ConfigManager.csproj").Build();
         }
 
         public void Run(string package)
@@ -26,11 +26,9 @@ namespace ConfigManager
                 }
                 case "update":
                 {
-                    var props = _config.GetSection("Project").GetSection("PropertyGroup").GetChildren().ToList();
-
                     Bash("sudo wget https://github.com/FiodorTretyakov/ConfigManager/raw/master/builds/ConfigManager.1.0.0.ubuntu.14.04-x64.deb"); //Download
                     Bash("sudo apt-get install -f"); //Get missed packages if any
-                    Bash($"sudo dpkg -i ConfigManager.{props.First(n => n.Key == "VersionPrefix")}-{props.First(n => n.Key == "VersionSuffix")}.ubuntu.14.04-x64.deb");
+                    Bash($"sudo dpkg -i ConfigManager.{GetVersion()}.ubuntu.14.04-x64.deb");
                     break;
                 }
                 case "apache2":
@@ -42,6 +40,12 @@ namespace ConfigManager
                     break;
                 }
             }
+        }
+
+        public string GetVersion()
+        {
+            var props = _config.GetSection("Project").GetSection("PropertyGroup").GetChildren().ToList();
+            return $"{props.First(n => n.Key == "VersionPrefix")}-{props.First(n => n.Key == "VersionSuffix")}";
         }
 
         public void Bash(string cmd)
--- Test/UnitTest.cs
+++ Test/UnitTest.cs
@@ -1,3 +1,5 @@
+using System;
+using ConfigManager;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Test
@@ -8,6 +10,11 @@ namespace Test
         [TestMethod]
         public void TestMethod1()
         {
+            var v = new Version(new Terminal().GetVersion());
+            Assert.AreEqual(v.Major, 0);
+            Assert.AreEqual(v.Minor, 0);
+            Assert.AreEqual(v.Build, 1);
+            Assert.AreEqual(v.Revision, 3);
         }
     }
 }
