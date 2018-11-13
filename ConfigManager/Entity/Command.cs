85e32fc873abd9788c510ab1804d967a82de1f88
Fiodor Tretyakov <fiodor.tretyakov@gmail.com>
2018-11-13 12:37:17 +0000

    Added base entity classes.

---
+++ ConfigManager/Entity/Command.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace ConfigManager.Entity
+{
+    public class Command
+    {
+
+        [JsonProperty("name", Required = Required.Always)]
+        public string Name;
+
+        [JsonProperty("desc")]
+        public string Description;
+    }
+}
---
+++ ConfigManager/Entity/Package.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ConfigManager.Entity
+{
+    public class Package : Command
+    {
+        [JsonProperty("dep", ItemIsReference = true, NullValueHandling = NullValueHandling.Include)]
+        public List<string> Dependencies;
+    }
+}
