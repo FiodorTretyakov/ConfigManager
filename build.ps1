04c34959db7026f91609e0b9cdbe251089c8c10f
Fiodor Tretyakov <fiodor.tretyakov@gmail.com>
2018-11-13 21:19:44 +0000

    Fix for the build.

--- build.ps1
+++ build.ps1
@@ -3,8 +3,8 @@ $runtime = "ubuntu.14.04-x64"
 $projectName = "ConfigManager"
 $projectFile = "$projectName\$projectName.csproj"
 [xml]$xml = Get-Content $projectFile
-$version = [version]$xml.Project.PropertyGroup.VersionPrefix
-$xml.Project.PropertyGroup.VersionPrefix = [string](New-Object -TypeName System.Version -ArgumentList $version.Major,$version.Minor,$version.Build, ($version.Revision + 1))
+$version = [version]$xml.Project.PropertyGroup[0].VersionPrefix
+$xml.Project.PropertyGroup[0].VersionPrefix = [string](New-Object -TypeName System.Version -ArgumentList $version.Major,$version.Minor,$version.Build, ($version.Revision + 1))
 $xml.Save("$projectFile")
 
 Set-Location "Test"
@@ -13,4 +13,5 @@ dotnet test "Test.csproj" /p:CollectCoverage=true
 Set-Location "..\$projectName"
 dotnet deb -c Release -r $runtime -f netcoreapp2.1
 Copy-Item ".\bin\Release\$runtime\$projectName.$prefix-$suffix.$runtime.deb" "..\builds\$projectName.$prefix.$suffix.$runtime.deb"
+
 Set-Location ..
